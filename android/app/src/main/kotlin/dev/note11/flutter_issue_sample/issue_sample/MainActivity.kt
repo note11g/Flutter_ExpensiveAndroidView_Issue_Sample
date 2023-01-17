4a1266beaa5613453c4426d616391d5bc1f466f9
note11 <note11@kakao.com>
2023-01-17 15:53:46 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/dev/note11/flutter_issue_sample/issue_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.note11.flutter_issue_sample.issue_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,249 @@
+import 'dart:developer' as dev;
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: IssueTestPage(),
+    );
+  }
+}
+
+class IssueTestPage extends StatefulWidget {
+  const IssueTestPage({Key? key}) : super(key: key);
+
+  @override
+  State<IssueTestPage> createState() => _IssueTestPageState();
+}
+
+class _IssueTestPageState extends State<IssueTestPage> {
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final widgetSize = Size(mediaQuery.size.width - 60, mediaQuery.size.height - 120);
+    final pixelRatio = mediaQuery.devicePixelRatio;
+    final physicalSize = Size(widgetSize.width * pixelRatio, widgetSize.height * pixelRatio);
+
+    dev.log("w/h : ${physicalSize.width} / ${physicalSize.height} "
+        "(logical : ${widgetSize.width} / ${widgetSize.height})",
+        name: "FlutterGoogleMapSizeMeasure");
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF343945),
+      body: Center(
+        child: Container(
+          width: widgetSize.width,
+          height: widgetSize.height,
+          color: Colors.greenAccent,
+          child: _googleMapSection(),
+        ),
+      ),
+    );
+  }
+
+  Widget _googleMapSection() {
+    return GoogleMap(
+        onMapCreated: (controller) {
+          controller.setMapStyle(_darkMapStyle);
+        },
+        initialCameraPosition:
+            const CameraPosition(target: LatLng(37.5665, 126.9780), zoom: 17));
+  }
+
+  static const _darkMapStyle = '''
+[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+]
+  ''';
+}
